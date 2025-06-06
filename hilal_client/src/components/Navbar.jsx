e0d59e02acfef06b1669dda442a3df0957afbf68
Arsalan <arsalan.online24@gmail.com>
2025-06-06 22:11:22 +0500

    link added in navbar sign in button

--- hilal_client/src/components/Navbar.jsx
+++ hilal_client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaChevronDown } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import Logo from "../assets/hilal-logo.svg";
 
 const Navbar = () => {
@@ -36,9 +37,11 @@ const Navbar = () => {
 
                 {/* Action Buttons */}
                 <div className="flex ml-auto space-x-2 text-[14px] lg:text-[16px] mt-4 lg:mt-0">
-                    <button className="w-28 lg:w-32 bg-white text-[#DF1600] p-2 lg:p-3 font-bold border border-white cursor-pointer">
-                        SIGN IN
-                    </button>
+                    <Link to="/admindashboard/articles">
+                        <button className="w-28 lg:w-32 bg-white text-[#DF1600] p-2 lg:p-3 font-bold border border-white cursor-pointer">
+                            SIGN IN
+                        </button>
+                    </Link>
                     <button className="w-28 lg:w-32 bg-white text-[#DF1600] p-2 lg:p-3 font-bold border border-white cursor-pointer">
                         SUBSCRIBE
                     </button>
