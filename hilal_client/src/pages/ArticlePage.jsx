8c07423b9a830241e2ef6959356a19b103a46552
muhammad-zeeshan-zia <zeeshanzia1270@gmail.com>
2025-08-09 16:16:01 +0500

    changed credee

--- hilal_client/src/pages/ArticlePage.jsx
+++ hilal_client/src/pages/ArticlePage.jsx
@@ -40,6 +40,15 @@ export default function ArticlePage() {
   });
 
   const handleSubmitComment = async () => {
+    if (!rating) {
+      alert("Rating is required.");
+      return;
+    }
+    if (!comment.trim()) {
+      alert("Comment is required.");
+      return;
+    }
+
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/create-comment/`, {
         comment,
